e4fec7332cbdd87b379e48c50b25c0b091e8204f
Lil Kev <kjolly544@gmail.com>
2024-04-13 00:24:58 -0400

    Update RewardGUI.php

--- src/GrosserZak/PortableCrates/Utils/RewardGUI.php
+++ src/GrosserZak/PortableCrates/Utils/RewardGUI.php
@@ -19,9 +19,9 @@ class RewardGUI extends InvMenu {
         self::$contents = $contents;
         parent::__construct(InvMenuHandler::getTypeRegistry()->get(InvMenuTypeIds::TYPE_DOUBLE_CHEST));
         parent::setListener(function (InvMenuTransaction $transaction) : InvMenuTransactionResult {
-            $pageNumber = $transaction->getAction()->getInventory()->getItem(49)->getNamedTag()->getCompoundTag("PortableCrates")?->getCompoundTag("RewardGUI")?->getByte("PageNumber");
+            $pageNumber = $transaction->getAction()->getInventory()->getItem(49)->getNamedTag()->getCompoundTag("RebirthCrates")?->getCompoundTag("RewardGUI")?->getByte("PageNumber");
             $clickedItem = $transaction->getItemClicked();
-            if(($nbt = $clickedItem->getNamedTag()->getCompoundTag("PortableCrates")?->getCompoundTag("RewardGUI")?->getTag("GoToPage")) !== null) {
+            if(($nbt = $clickedItem->getNamedTag()->getCompoundTag("RebirthCrates")?->getCompoundTag("RewardGUI")?->getTag("GoToPage")) !== null) {
                 $goTo = $nbt->getValue();
                 $this->renderItemsPage(($goTo === "Previous" ? $pageNumber-1 : ($goTo === "Next" ? $pageNumber+1 : 0)));
             }
